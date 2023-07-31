9ed5fb3d6cabfa22da4581bb2e89851f6f00dcb2
HealingDoveFoundation <141007939+HealingDoveFoundation@users.noreply.github.com>
2023-07-31 08:46:15 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Contact-HDF</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Free Website Template" name="keywords">
+        <meta content="Free Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/HDF logo .png" rel="icon">
+
+        <!-- Google Font -->
+        <link href="https://fonts.google.com/speciman/roboto" rel="stylesheet">
+        
+        <!-- CSS Libraries -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+        
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+       <!-- Top Bar Start -->
+       <div class="top-bar d-none d-md-block">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="top-bar-left">
+                        <div class="text">
+                            <i class="fa fa-phone-alt"></i>
+                            <p>(+91) 7045688978</p>
+                        </div>
+                        <div class="text">
+                            <i class="fa fa-envelope"></i>
+                            <p>healingdovefoundation@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="top-bar-right">
+                        <div class="social">
+                            <a href=""><i class="fab fa-twitter"></i></a>
+                            <a href=""><i class="fab fa-facebook-f"></i></a>
+                            <a href=""><i class="fab fa-linkedin-in"></i></a>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Top Bar End -->
+
+    
+        <!-- Nav Bar Start -->
+        <div class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container-fluid">
+                <img class="navbar-img" src="img/HDF logo .png" alt="Image" style="height: 100px; width: 100px; padding: 10px;">
+                <a href="index.html" class="navbar-brand">Healing Dove Foundation</a><br>
+                <button type="button" class="navbar-toggler bg-dark" data-toggle="collapse" data-target="#navbarCollapse" >
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto">
+                        <a href="index.html" class="nav-item nav-link ">Home</a>
+                        <a href="about.html" class="nav-item nav-link">About</a>
+                        <a href="causes.html" class="nav-item nav-link">Work We Do</a>
+                        
+                        <a href="get-involved.html" class="nav-item nav-link">Get Involved</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu">
+                                <a href="features.html" class="dropdown-item">Feature Gallery</a>
+                                <a href="multimedia.html" class="dropdown-item">Multimedia</a>
+                                <a href="blog.html" class="dropdown-item">Blogs</a>
+                                <a href="testimonials.html" class="dropdown-item">Testimonial</a>
+                            </div>
+                        </div>
+                        <a href="contact.php" class="nav-item nav-link active">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Nav Bar End -->
+        
+        <!-- Page Header Start -->
+        <div class="page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Contact Us</h2>
+                    </div>
+                    <div class="col-12">
+                        <a href="index.html">Home</a>
+                        <a href="">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Header End -->
+        
+
+        
+        <!-- Contact Start -->
+        <div class="contact">
+            <div class="container">
+                <div class="section-header text-center">
+                    <p>Get In Touch</p>
+                    <h2>Contact for any query</h2>
+                </div>
+                
+                <div class="contact-form">
+                        <div id="success"></div>
+                        <form method="POST" name="sentMessage" id="contactForm">
+                            <div class="control-group">
+                                <input type="text" class="form-control" name="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <input type="email" class="form-control" name="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <input type="text" class="form-control" name="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="control-group">
+                                <textarea class="form-control" name="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div>
+                                <button class="btn btn-custom" type="submit" name="submit">Send Message</button>
+                            </div>
+                        </form>
+                    </div>
+            </div>
+        </div>
+        
+  
+
+  
+
+        <!-- Contact End -->
+
+ <!-- Footer Start -->
+ <div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-6">
+                <div class="footer-contact">
+                    <h2>Our Head Office</h2>
+                    <p><i class="fa fa-map-marker-alt"></i>2, Floor-Grd, Plot-37, Keni Apartment, Lt. Dilip Gupte Marg, Shivaji Park, Mahim, Mumbai-400016</p>
+                    <p><i class="fa fa-phone-alt"></i>(+91) 7045688978</p>
+                    <p><i class="fa fa-envelope"></i>healingdovefoundation@gmail.com</p>
+                    <div class="footer-social">
+                        <a class="btn btn-custom" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-custom" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-custom" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-custom" href=""><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-custom" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-link">
+                    <h2>Popular Links</h2>
+                    <a href="">About Us</a>
+                    <a href="">Contact Us</a>
+                    <a href="">Work We Do</a>
+                    <a href="">Annual Project Reports</a>
+                    <a href="">Latest Blog</a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <div class="footer-link">
+                    <h2>Useful Links</h2>
+                    <a href="">Terms of use</a>
+                    <a href="">Privacy policy</a>
+                  
+                    <a href="">Help</a>
+                    
+                </div>
+            </div>
+          
+        </div>
+    </div>
+    <div class="container copyright">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; <a href="#">healing Dove Foundation</a>, All Rights Reserved.</p>
+            </div>
+            <div></div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+<!-- Back to top button -->
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+<!-- Pre Loader -->
+<div id="loader" class="show">
+    <div class="loader"></div>
+</div>
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="lib/waypoints/waypoints.min.js"></script>
+<script src="lib/counterup/counterup.min.js"></script>
+<script src="lib/parallax/parallax.min.js"></script>
+
+<!-- Contact Javascript File -->
+<script src="mail/jqBootstrapValidation.min.js"></script>
+<!--<script src="mail/contact.js"></script>-->
+
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
+<script>
+    function error(){
+    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append('</div>');
+                    $('#contactForm').trigger("reset");
+    }
+    function success() {
+        $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                            .append("</button>");
+                    $('#success > .alert-success')
+                            .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                            .append('</div>');
+                    $('#contactForm').trigger("reset");
+    }
+</script>
+</body>
+</html>
+<?php
+require 'mail/PHPMailerAutoload.php';
+if(isset($_POST['submit'])){
+
+    if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        echo "<script type='text/javascript'>
+        error();
+        </script>";
+        exit();
+      }
+      $name = strip_tags(htmlspecialchars($_POST['name']));
+      $email = strip_tags(htmlspecialchars($_POST['email']));
+      $subject = strip_tags(htmlspecialchars($_POST['subject']));
+      $message = strip_tags(htmlspecialchars($_POST['message']));
+      $servername = "localhost";
+      $username = "healingd_database";
+      $password = "HDFData@123";
+      $dbname = "healingd_queries";
+    
+    try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "insert into Queries(Name, Email, Subject, Message) values ('$name', '$email', '$subject','$message')";
+      // use exec() because no results are returned
+      $conn->exec($sql);
+      echo "<script type='text/javascript'>
+      success();
+      </script>" ;
+      $msg = "Name: '$name' \nEmail: '$email' \nSubject: '$subject' \nMessage: '$message' " ;
+      mail("healingdovefoundation@gmail.com",$subject,$msg);
+   
+    } catch(PDOException $e) {
+        echo $e;
+      echo "<script type='text/javascript'>
+      error();
+      </script>";
+    }
+
+}
+$conn = null;   
+?>
